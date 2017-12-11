42d06e768e09a8c526d76364fc63ac0a0a80078f
Alex Kuznecov <kuzniecov@mail.ru>
2017-12-12 01:17:22 +0300

    Initial config of user module

---
+++ user/src/main/java/by/home/user/UserModuleStarter.java
@@ -0,0 +1,16 @@
+package by.home.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by alek on 12.12.17.
+ */
+@SpringBootApplication
+public class UserModuleStarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserModuleStarter.class, args);
+    }
+
+}
---
+++ user/src/test/java/by/home/user/UserModuleTest.java
@@ -0,0 +1,19 @@
+package by.home.user;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by alek on 12.12.17.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UserModuleTest {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
