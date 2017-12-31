1611677fbbc5eded50dc7652ed379b78535e6923
Alex Kuznecov <kuzniecov@mail.ru>
2017-12-31 03:33:51 +0300

    start working with mongo template

--- anime/src/main/java/by/home/eao/AnimeEAO.java
+++ anime/src/main/java/by/home/eao/AnimeEAO.java
@@ -1,7 +1,12 @@
 package by.home.eao;
 
+import by.home.model.Anime;
+
 /**
  * Created by alek on 31.12.17.
  */
 public interface AnimeEAO {
+
+    void insert(Anime anime);
+
 }
--- anime/src/main/java/by/home/eao/impl/AnimeEAOImpl.java
+++ anime/src/main/java/by/home/eao/impl/AnimeEAOImpl.java
@@ -1,9 +1,21 @@
 package by.home.eao.impl;
 
 import by.home.eao.AnimeEAO;
+import by.home.model.Anime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 /**
- * Created by alek on 31.12.17.
+ * @author alek
+ * @since 0.0.1
  */
 public class AnimeEAOImpl implements AnimeEAO {
+
+    @Autowired
+    private MongoTemplate template;
+
+    @Override
+    public void insert(Anime anime) {
+        template.insert(anime);
+    }
 }
